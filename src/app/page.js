cedb47e81bc4a2090d73e0366d817784bb5ea549
Jwill617Boston <77422031+Jwill617Boston@users.noreply.github.com>
2023-07-19 09:27:55 -0400

    homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,24 @@
-import Image from 'next/image'
-import { env } from "node:process";
+import Image from "next/image";
 
 export default function Home() {
    return (
       <main className="flex min-h-screen flex-col items-center justify-center p-24">
-         <h1>Helloo</h1>
+         <section className="w-screen h-screen flex items-center justify-center ">
+            <h1 className="m-3 p-4 bg-indigo-500">Section 1</h1>
+            <h1 className="m-3 p-4 bg-orange-800">Section 1</h1>
+         </section>
+         <section className="w-screen h-screen flex items-center justify-center ">
+            <h1 className="m-3 p-4 bg-indigo-500">Section 2</h1>
+            <h1 className="m-3 p-4 bg-orange-800">Section 2</h1>
+         </section>
+         <section className="w-screen h-screen flex items-center justify-center ">
+            <h1 className="m-3 p-4 bg-indigo-500">Section 3</h1>
+            <h1 className="m-3 p-4 bg-orange-800">Section 3</h1>
+         </section>
+         <section className="w-screen h-screen flex items-center justify-center ">
+            <h1 className="m-3 p-4 bg-indigo-500">Section 4</h1>
+            <h1 className="m-3 p-4 bg-orange-800">Section 4</h1>
+         </section>
       </main>
    );
 }
